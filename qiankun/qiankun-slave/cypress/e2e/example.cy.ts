81a56303fcc2caa67bf3cc154ff151e5a9ba5732
cloudyan <1395093509@qq.com>
2023-07-24 14:06:41 +0800

    feat: update deps

--- qiankun/qiankun-slave/cypress/e2e/example.cy.ts
+++ qiankun/qiankun-slave/cypress/e2e/example.cy.ts
@@ -6,7 +6,7 @@ describe('QianKun Plugin', () => {
   it('can navigate to slave', () => {
     // contains https://docs.cypress.io/api/commands/contains
     cy.visit('/home');
-    cy.get('button').click();
+    cy.get('a[href*="/slave/home"]').click();
 
     cy.contains('Slave Home Page');
   });
@@ -18,6 +18,15 @@ describe('QianKun Plugin', () => {
     cy.contains('count:0');
     cy.get('button').click();
     cy.contains('count:1');
+  });
+
+  it('support app using module-federation', () => {
+    cy.visit('/slave-mf/dynamic-import');
+
+    cy.contains('remote Counter');
+    cy.contains('remote hooks counter10');
+    cy.get('[data-testid="remote-button"]').click();
+    cy.contains('remote hooks counter11');
   });
 
   describe('manual loaded app', function () {
@@ -33,6 +42,23 @@ describe('QianKun Plugin', () => {
       cy.contains('count:0');
       cy.get('button').click();
       cy.contains('count:1');
+    });
+  });
+
+  describe('microApp route first', () => {
+    it('not hit indexApp route', () => {
+      // contains https://docs.cypress.io/api/commands/contains
+      cy.visit('/nav');
+
+      cy.contains('never seen').should('not.exist');
+    });
+  });
+
+  describe('microApp route prepend ok', () => {
+    it('hit microApp route', () => {
+      cy.visit('/prefix/nav');
+
+      cy.contains('goto slave app2');
     });
   });
 
