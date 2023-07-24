fb28fbc4d103e7735c6769457cb277aa8b2ea7aa
cloudyan <1395093509@qq.com>
2023-07-24 14:26:26 +0800

    feat: update master-umi4

--- qiankun3/master-umi4/src/pages/Access/index.tsx
+++ qiankun3/master-umi4/src/pages/Access/index.tsx
@@ -1,5 +1,6 @@
 import { PageContainer } from '@ant-design/pro-components';
 import { Access, useAccess } from '@umijs/max';
+// MicroAppWithMemoHistory
 import { Button } from 'antd';
 
 const AccessPage: React.FC = () => {
@@ -14,6 +15,7 @@ const AccessPage: React.FC = () => {
       <Access accessible={access.canSeeAdmin}>
         <Button>只有 Admin 可以看到这个按钮</Button>
       </Access>
+      {/* <MicroAppWithMemoHistory name={"sub-app-2"}/> */}
     </PageContainer>
   );
 };
